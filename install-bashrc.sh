51ddfe880ce26c8edfc333648ce9b0984b748c03
Guillaume Isabelle <jgi@jgwill.com>
2019-04-11 16:21:53 -0400

    fix install

--- install-bashrc.sh
+++ install-bashrc.sh
@@ -2,8 +2,8 @@
 
 # Install aliases into the .bashrc after installing the Package
 
-cd 
-mkdir -p apps && cd  $_ && mkdir ngx-translate-helpercli && cd $_ && npm init --y && npm i ngx-translate-helpercli --save
-echo 'alias newstring="~/apps/ngx-translate-helpercli/node_modules/.bin/ngx-translate-helpercli newstring "'>>~/.bashrc
-echo 'alias ngxtr="~/apps/ngx-translate-helpercli/node_modules/.bin/ngx-translate-helpercli "'>>~/.bashrc
+cd && \
+mkdir -p apps && cd  $_ && mkdir ngx-translate-helpercli && cd $_ && npm init --y && npm i ngx-translate-helpercli --save && \
+echo 'alias newstring="~/apps/ngx-translate-helpercli/node_modules/.bin/ngx-translate-helpercli newstring "'>>~/.bashrc && \
+echo 'alias ngxtr="~/apps/ngx-translate-helpercli/node_modules/.bin/ngx-translate-helpercli "'>>~/.bashrc &&\
 echo "New command newstring and ngxtr are created"
