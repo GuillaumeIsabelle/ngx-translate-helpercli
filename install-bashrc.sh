a212f3b0f861f0b16ae93d46e835fb9e28c70e22
Guillaume Isabelle <jgi@jgwill.com>
2019-04-11 16:27:32 -0400

    fix installer

--- install-bashrc.sh
+++ install-bashrc.sh
@@ -4,6 +4,6 @@
 
 cd && \
 mkdir -p apps/ngx-translate-helpercli && cd $_ && npm init --y && npm i ngx-translate-helpercli --save && \
-echo 'alias newstring="~/apps/ngx-translate-helpercli/node_modules/.bin/ngx-translate-helpercli newstring "'>>~/.bashrc && \
-echo 'alias ngxtr="~/apps/ngx-translate-helpercli/node_modules/.bin/ngx-translate-helpercli "'>>~/.bashrc &&\
+echo 'alias newstring="~/apps/ngxtr/node_modules/.bin/ngx-translate-helpercli newstring "'>>~/.bashrc && \
+echo 'alias ngxtr="~/apps/ngxtr/node_modules/.bin/ngx-translate-helpercli "'>>~/.bashrc &&\
 echo "New command newstring and ngxtr are created"
