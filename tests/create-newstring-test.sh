63a3ac79ff73c219e081c4287b3072cd030f155d
Guillaume Isabelle <jgi@jgwill.com>
2019-04-11 15:42:53 -0400

    added tests

---
+++ tests/create-newstring-test.sh
@@ -0,0 +1,3 @@
+#create a new string TST and show it
+node ../ngx-translate-helpercli.js newstring "Hello worlddof software developper."    "HOME.TST" ; cat src/assets/i18n/fr.json | grep TST ; cat src/assets/i18n/de.json | grep TST
+
